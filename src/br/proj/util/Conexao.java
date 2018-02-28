7292e10851cb40711ed78e1bb6790f3d9da57230
Aluno Fiar Logatti <aluno@fiar.local>
2018-02-28 20:19:06 -0300

    criacao classe conexao

---
+++ src/br/proj/util/Conexao.java
@@ -0,0 +1,25 @@
+package br.proj.util;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+
+	public static java.sql.Connection getConnection(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = null;
+			con = (Connection)
+					DriverManager.getConnection("jdb:mysql://localhost/cadastro",
+							"root", "1234");
+			return con;
+		}catch(SQLException e) {
+			throw new RuntimeException(e);
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
