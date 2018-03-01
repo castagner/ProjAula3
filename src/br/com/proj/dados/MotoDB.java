b6488bfc6d154fa56a03cf065dae5c59fae815bf
Aluno Fiar Logatti <aluno@fiar.local>
2018-02-28 21:42:22 -0300

    criar banco

--- src/br/com/proj/dados/MotoDB.java
+++ src/br/com/proj/dados/MotoDB.java
@@ -1,14 +1,23 @@
 package br.com.proj.dados;
 
 import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import br.com.proj.entidade.Moto;
+import br.com.proj.util.Conexao;
 
 public class MotoDB implements IMotoDB{
 
+private Connection  con;
+private PreparedStatement ps;
+
 	public MotoDB() {
 		//AbERTURA DE CONEXAO COM A BASE DE DADOS
+		con =  Conexao.getConnection();
 		
+				
 	}
 	
 	public boolean insert (Moto moto) {
@@ -17,7 +26,35 @@ public class MotoDB implements IMotoDB{
 		 System.out.println("Ano: " +moto.getAno());
 		
 		// implementar insert banco de dado0s
+		 
+		boolean status = false;
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("INSERT INTO MOTO");
+		sb.append(" (id, descricao, ano) values (?, ? ,? )");
+		
+		try {
+		ps = this.con.prepareStatement(sb.toString());
+		
+		ps.setInt(1, moto.getId());
+		ps.setString(2, moto.getDescricao());
+		ps.setInt(3, moto.getAno());
+		
+		 ps.execute();
+		
 		return true;
+		
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				con.close();
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return false;
 	}
 
 	@Override
